0a903cf88430f804991fbfca7f6fee273f2547fe
lenor4ik <leno4kamay08051987@gmail.com>
2024-01-19 18:58:45 +0100

    dz-9

---
+++ src/js/2-form.js
@@ -0,0 +1,49 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+
+function saveToLS(key, value) {
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+    const data = localStorage.getItem(key);
+    try {
+        const result = JSON.parse(data);
+        return result;
+    } catch {
+        return data;
+    }
+}
+
+form.addEventListener('input', (e) => {
+    const userEmail = form.elements.email.value.trim();
+    const userMessage = form.elements.message.value.trim();
+
+    const data = {
+        email: userEmail,
+        message: userMessage,
+    };
+    saveToLS(STORAGE_KEY, data);
+});
+
+function restoreData() {
+    const data = loadFromLS(STORAGE_KEY) || {};
+    form.elements.email.value = data.email || '';
+    form.elements.message.value = data.message || '';
+}
+
+restoreData();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = loadFromLS(STORAGE_KEY) || {};
+    if (data.email && data.message) {
+    localStorage.removeItem(STORAGE_KEY)
+    form.reset();  
+    } 
+
+    console.log(data);
+})
